c2dead18e88ac962776f24b8eb57ceb6f1a5a4ea
OGURA_Daiki <8hachibee125@gmail.com>
2018-03-25 21:13:11 +0900

    first neural network

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import numpy as np
 from matplotlib import pylab as plt
 
-from .src import exp
+from src import exp
 
 
-class Chap_3_3
+class Chap_3_3:
     def confirm_sigmoid():
         x = np.array([-1.0, 1.0, 2.0])
         y = exp.sigmoid(x)
@@ -40,3 +40,40 @@ class Chap_3_3
         Z1 = exp.sigmoid(A1)
         print(A1)
         print(Z1)
+
+
+def init_network():
+    return {
+        'W1': np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]),
+        'b1': np.array([0.1, 0.2, 0.3]),
+
+        'W2': np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]),
+        'b2': np.array([0.1, 0.2]),
+
+        'W3': np.array([[0.1, 0.3], [0.2, 0.4]]),
+        'b3': np.array([0.1, 0.2]),
+    }
+
+
+def forward(network, x):
+    W1, W2, W3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
+
+    a1 = np.dot(x, W1) + b1
+    z1 = exp.sigmoid(a1)
+
+    a2 = np.dot(z1, W2) + b2
+    z2 = exp.sigmoid(a2)
+
+    a3 = np.dot(z2, W3) + b3
+    y = exp.identity(a3)
+    return y
+
+
+if __name__ == '__main__':
+    network = init_network()
+    x = np.array([1.0, 0.5])
+    y = forward(network, x)
+    print(y)
+    assert y[0] == 0.3168270764110298, y[0]
+    assert y[1] == 0.6962790898619668, y[1]
