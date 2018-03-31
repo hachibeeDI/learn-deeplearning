683ad03c05efed5e06aa968056cfb703264ecbc7
OGURA Daiki <8hachibee125@gmail.com>
2018-03-31 21:58:10 +0900

    softmax

--- src/exp.py
+++ src/exp.py
@@ -15,3 +15,27 @@ def relu(x):
 
 def identity(x):
     return x
+
+
+def _softmax_could_overflow(a):
+    exp_a = np.exp(a)
+    y = exp_a / np.sum(exp_a)
+    return y
+
+
+def softmax(a):
+    """
+    The summed value is 1.0 whatever argument is.
+    That means we can use each values as probability.
+
+    >>> a = np.array([0.3, 2.9, 4.0])
+    >>> y = softmax(a)
+
+    >>> np.sum(y)
+    1.0
+    """
+    c = np.max(a)
+
+    exp_a = np.exp(a - c)
+    y = exp_a / np.sum(exp_a)
+    return y
